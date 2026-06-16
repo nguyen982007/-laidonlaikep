6626a06344ad3a76568bac42cd5fd6d43ec0bd1c
nguyen982007 <twnguyenn109@gmail.com>
2026-06-16 14:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+st.image("
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_Nguyễn Ngọc Thảo Nguyên")
 
 # Nhập dữ liệu
 C = st.number_input(
